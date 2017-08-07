dfb0ea3f9fdd0a0db255acee8b11c6d4b4ab7673
Gustavo Guzman Office <gguzman@forcemsystems.com>
2017-08-07 13:06:36 -0500

    Debug desarrollos

--- app/Http/Controllers/PropiedadesController.php
+++ app/Http/Controllers/PropiedadesController.php
@@ -265,11 +265,11 @@ class PropiedadesController extends Controller
            if($request->logo){ $request->logo->store('public/logos'); }
 
             $desarrollo = new Desarrollos;
-                    // if($request->logo){ $desarrollo->logo=$request->logo->hashName(); }
+            if($request->logo){ $desarrollo->logo=$request->logo->hashName(); }
             $desarrollo->fill($request->all());
-            // $desarrollo->title=$request->title;
-            // $desarrollo->latitud=$request->latitud;
-            // $desarrollo->link_video=$link_video;
+            $desarrollo->title=$request->title;
+            $desarrollo->latitud=$request->latitud;
+            $desarrollo->link_video=$link_video;
             $desarrollo->save();        
             $desarrollo_id = $desarrollo->id;
             
